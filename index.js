fa758da71bd4f4da3c5af1cbd78bd3f5ed98c775
shahariartushar <shahariartushar@gmail.com>
2022-02-15 16:22:16 +0100

    code refactor

--- index.js
+++ index.js
@@ -1,81 +1,13 @@
 const Joi = require('joi');
-
+const genres = require('./routes/genres'); 
 const express = require('express');
+
 const app = express();
 
 app.use(express.json());
+app.use('/api/genres', genres);
 
 //port
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
-//genres
-const genres =[
-    { id: 1, name: 'Action'},
-    { id: 2, name: 'Comedy'},
-    { id: 3, name: 'Drama'},
-    { id: 4, name: 'Fantasy'},
-    { id: 5, name: 'Horror'},
-    { id: 6, name: 'Mystery'},
-    { id: 7, name: 'Romance'},
-    { id: 8, name: 'Thriller'}
-];
-
-//get all the genres
-app.get('/api/genres', (req, res) => {
-    res.send(genres)
-});
-
-//get a genre by id
-app.get('/api/genres/:id', (req, res)=>{
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if (!genre)    return res.status(404).send('Genre Id not found');
-    
-    res.send(genre);
-});
-
-//create a genre
-app.post('/api/genres', (req, res)=>{
-    const {error} = validateCourse(req.body);
-    if(error)   return  res.status(400).send(error.details[0].message);
-
-    const genre =  {
-        id: genres.length + 1,
-        name: req.body.name
-
-    };
-    genres.push(genre);
-    
-    res.send(genre);
-});
-
-//update a genre
-app.put('/api/genres/:id', (req, res)=>{
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if (!genre)    return res.status(404).send('Genre Id not found');
-
-    const {error} = validateCourse(req.body);
-    if(error)   return res.status(400).send(error.details[0].message);
-
-    genre.name = req.body.name;
-    
-    res.send(genre);
-});
-
-app.delete('/api/genres/:id', (req, res)=>{
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if (!genre)    return res.status(404).send('Genre Id not found');
-
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-
-    res.send(genre);
-});
-
-function validateCourse(genre){
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    
-    return  Joi.validate(genre, schema);
-}
